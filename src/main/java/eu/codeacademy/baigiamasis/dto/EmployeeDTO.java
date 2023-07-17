4c0fade42fea4ca9cc07be17a61e32935298541a
Karolis Kersys <kersys.karolis@inbox.lt>
2023-07-17 22:42:50 +0300

    RESTful api: created Employee data-transfer object

---
+++ src/main/java/eu/codeacademy/baigiamasis/dto/EmployeeDTO.java
@@ -0,0 +1,17 @@
+package eu.codeacademy.baigiamasis.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class EmployeeDTO {
+    private Long id;
+    private String username;
+    private String name; 
+    private String email;
+}
