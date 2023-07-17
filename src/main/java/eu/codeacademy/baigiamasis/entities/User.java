7cf11d8144207f808eeac18f60d5a21043106bbd
Karolis Kersys <kersys.karolis@inbox.lt>
2023-07-17 20:48:50 +0300

    RESTful api: created User entity using lombok

---
+++ src/main/java/eu/codeacademy/baigiamasis/entities/User.java
@@ -0,0 +1,50 @@
+package eu.codeacademy.baigiamasis.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private String name;
+    @Column
+    private String surname;
+    @Column
+    private Integer phoneNumber;
+    @Column
+    private String email;
+    @Column
+    private LocalDateTime createdAt;
+    @Column
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
