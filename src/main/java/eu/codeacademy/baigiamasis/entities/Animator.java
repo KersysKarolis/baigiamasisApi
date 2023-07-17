a90a0017192709a34b1e86814a8515d06090f1bf
Karolis Kersys <kersys.karolis@inbox.lt>
2023-07-17 22:07:22 +0300

    RESTful api: created Animator entity using lombok

--- src/main/java/eu/codeacademy/baigiamasis/entities/Animator.java
+++ src/main/java/eu/codeacademy/baigiamasis/entities/Animator.java
@@ -1,4 +1,39 @@
 package eu.codeacademy.baigiamasis.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "animators")
 public class Animator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String surname;
+    @Column
+    private String email;
+    @Column
+    private LocalDateTime createdAt;
+    @Column
+    private LocalDateTime updatedAt;
+    @PrePersist
+    void prePersist(){
+        this.createdAt = LocalDateTime.now();
+    }
+    @PreUpdate
+    void preUpdate (){
+        this.updatedAt = LocalDateTime.now();
+    }
 }
