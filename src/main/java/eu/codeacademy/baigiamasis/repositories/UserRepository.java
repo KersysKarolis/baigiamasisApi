07910b177ab1d29be949620d77b780f67a100eac
Karolis Kersys <kersys.karolis@inbox.lt>
2023-07-17 22:32:50 +0300

    RESTful api: created JPA Repositories for entities

---
+++ src/main/java/eu/codeacademy/baigiamasis/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package eu.codeacademy.baigiamasis.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
