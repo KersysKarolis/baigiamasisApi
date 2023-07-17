5a10e107252956ad6e486f97fb27c6fcb5d33142
Karolis Kersys <kersys.karolis@inbox.lt>
2023-07-17 20:34:11 +0300

    RESTful api: security configuration added

---
+++ src/test/java/eu/codeacademy/baigiamasis/BaigiamasisApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.codeacademy.baigiamasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaigiamasisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
