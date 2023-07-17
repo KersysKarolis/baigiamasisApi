e403c895b316a2df84936b0cf53657aaec562e58
Karolis Kersys <kersys.karolis@inbox.lt>
2023-07-17 22:37:47 +0300

    RESTful api: created User data-transfer object

---
+++ src/main/java/eu/codeacademy/baigiamasis/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package eu.codeacademy.baigiamasis.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String surname;
+    private String username;
+    private String email;
+    private Integer phone;
+    private List<OrderDTO> orderList;
+}
