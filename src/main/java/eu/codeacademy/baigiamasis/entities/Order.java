0cc2c0d670748012274af8fdf1003128e707f5e1
Karolis Kersys <kersys.karolis@inbox.lt>
2023-07-17 21:51:56 +0300

    RESTful api: created Order entity using lombok

---
+++ src/main/java/eu/codeacademy/baigiamasis/entities/Order.java
@@ -0,0 +1,35 @@
+package eu.codeacademy.baigiamasis.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private Integer orderNumber;
+   @Column
+    private LocalDateTime orderCreatedAt;
+   @Column
+    private LocalDateTime orderUpdatedAt;
+   @PrePersist
+    void prePersist(){
+       this.orderCreatedAt = LocalDateTime.now();
+   }
+   @PreUpdate
+    void preUpdate(){
+       this.orderUpdatedAt = LocalDateTime.now();
+   }
+}
