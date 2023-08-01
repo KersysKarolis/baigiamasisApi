b00303f56632fd670603e812bd0ba828659d3175
Karolis Kersys <kersys.karolis@inbox.lt>
2023-08-01 19:53:05 +0300

    RESTful api: added methods for pageing and sorting

--- src/main/java/eu/codeacademy/baigiamasis/repositories/EmployeeRepository.java
+++ src/main/java/eu/codeacademy/baigiamasis/repositories/EmployeeRepository.java
@@ -1,7 +1,10 @@
 package eu.codeacademy.baigiamasis.repositories;
 
 import eu.codeacademy.baigiamasis.entities.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Page<Employee> findAllByRole (Pageable pageable, String role);
 }
--- src/main/java/eu/codeacademy/baigiamasis/repositories/OrderRepository.java
+++ src/main/java/eu/codeacademy/baigiamasis/repositories/OrderRepository.java
@@ -1,8 +1,11 @@
 package eu.codeacademy.baigiamasis.repositories;
 
 import eu.codeacademy.baigiamasis.entities.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Page<Order> findAllByClient_Id(Pageable pageable, Long id);
 
 }
