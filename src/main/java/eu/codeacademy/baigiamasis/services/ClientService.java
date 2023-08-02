362b919570388d1b2a52369389aef45a3442a37a
Karolis Kersys <kersys.karolis@inbox.lt>
2023-08-02 22:14:12 +0300

    RESTful api: Injected MessageSource class which responsible for messages occured with exceptions. Updated exceptions with customs. Solved problem with password not being saved after password update.

--- src/main/java/eu/codeacademy/baigiamasis/services/ClientService.java
+++ src/main/java/eu/codeacademy/baigiamasis/services/ClientService.java
@@ -3,19 +3,23 @@ package eu.codeacademy.baigiamasis.services;
 import eu.codeacademy.baigiamasis.converters.ClientConverter;
 import eu.codeacademy.baigiamasis.converters.OrderConverter;
 import eu.codeacademy.baigiamasis.dto.ClientDTO;
-import eu.codeacademy.baigiamasis.dto.ClientPasswordChangeDTO;
+import eu.codeacademy.baigiamasis.dto.PasswordChangeDTO;
 import eu.codeacademy.baigiamasis.dto.CreateClientDTO;
 import eu.codeacademy.baigiamasis.entities.Client;
 import eu.codeacademy.baigiamasis.enumerators.Role;
+import eu.codeacademy.baigiamasis.exceptions.ObjectAlreadyExistsException;
+import eu.codeacademy.baigiamasis.exceptions.PasswordDoesNotMatchException;
 import eu.codeacademy.baigiamasis.repositories.ClientRepository;
+import eu.codeacademy.baigiamasis.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -25,6 +29,10 @@ public class ClientService {
     ClientRepository clientRepository;
     @Autowired
     PasswordEncoder passwordEncoder;
+    @Autowired
+    MessageSource messageSource;
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     public List<ClientDTO> getAllClients() {
         return ClientConverter.convertClientsToClientsDTO(clientRepository.findAll());
@@ -34,14 +42,16 @@ public class ClientService {
         return clientRepository.findById(id).get();
     }
 
-    public ClientDTO addClient(CreateClientDTO createClientDTO) {
-        if (createClientDTO.getFirstPassword().equals(createClientDTO.getSecondPassword())) {
+    public ClientDTO addClient(CreateClientDTO createClientDTO) throws ObjectAlreadyExistsException, PasswordDoesNotMatchException {
+        if(clientRepository.existsClientByUsername(createClientDTO.getUsername())){
+            throw new ObjectAlreadyExistsException(messageSource.getMessage("username.already.exists", null, LocaleContextHolder.getLocale()));
+        } else if(!(createClientDTO.getFirstPassword().equals(createClientDTO.getSecondPassword()))){
+            throw new PasswordDoesNotMatchException(messageSource.getMessage("password.does.not.match", null, LocaleContextHolder.getLocale()));
+        }else {
             Client client = ClientConverter.convertCreateClientDTOToClient(createClientDTO);
             client.setPassword(passwordEncoder.encode(client.getPassword()));
             clientRepository.saveAndFlush(client);
             return ClientConverter.convertClientToClientDTO(client);
-        } else {
-            throw new NullPointerException("Username and password fields must be filled");
         }
 
 
@@ -75,22 +85,23 @@ public class ClientService {
         return ClientConverter.convertClientToClientDTO(client);
     }
 
-    public void changeClientPassword(ClientPasswordChangeDTO password, Long id) {
+    public void changeClientPassword(PasswordChangeDTO password, Long id) throws PasswordDoesNotMatchException{
         Client client = getClientById(id);
         if (!(passwordEncoder.matches(password.getOldPassword(), client.getPassword()))) {
-            throw new InputMismatchException("Incorrect password provided");
+            throw new PasswordDoesNotMatchException(messageSource.getMessage("incorrect.password", null, LocaleContextHolder.getLocale()));
 
         } else if (password.getNewPassword().equals(password.getRepeatNewPassword())) {
-            throw new InputMismatchException("New passwords does not match");
+            throw new PasswordDoesNotMatchException(messageSource.getMessage("new.password.does.not.match", null, LocaleContextHolder.getLocale()));
         } else {
             client.setPassword(passwordEncoder.encode(password.getNewPassword()));
+           clientRepository.saveAndFlush(client);
         }
 
     }
 
     public void changeClientRole(String role, Long id) {
         Client client = getClientById(id);
-        if (role.equals("ADMIN")) {
+        if (role.equals(Role.ADMIN.name())) {
             client.setRole(Role.ADMIN);
         } else if (role.equals("EMPLOYEE")) {
             client.setRole(Role.EMPLOYEE);
--- src/main/java/eu/codeacademy/baigiamasis/services/EmployeeService.java
+++ src/main/java/eu/codeacademy/baigiamasis/services/EmployeeService.java
@@ -2,18 +2,23 @@ package eu.codeacademy.baigiamasis.services;
 
 import eu.codeacademy.baigiamasis.converters.EmployeeConverter;
 import eu.codeacademy.baigiamasis.converters.OrderConverter;
+import eu.codeacademy.baigiamasis.dto.PasswordChangeDTO;
 import eu.codeacademy.baigiamasis.dto.CreateEmployeeDTO;
 import eu.codeacademy.baigiamasis.dto.EmployeeDTO;
 import eu.codeacademy.baigiamasis.entities.Employee;
+import eu.codeacademy.baigiamasis.exceptions.ObjectAlreadyExistsException;
+import eu.codeacademy.baigiamasis.exceptions.PasswordDoesNotMatchException;
 import eu.codeacademy.baigiamasis.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.InputMismatchException;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class EmployeeService {
@@ -21,6 +26,8 @@ public class EmployeeService {
     EmployeeRepository employeeRepository;
     @Autowired
     PasswordEncoder passwordEncoder;
+    @Autowired
+    MessageSource messageSource;
 
     public List<EmployeeDTO> getAllEmployeesByRole(Pageable pageable, String role){
         Page<Employee> employeePage = null;
@@ -31,34 +38,52 @@ public class EmployeeService {
         }
         return EmployeeConverter.convertEmployeesToEmployeesDTO(employeePage);
     }
-    public EmployeeDTO getEmployeeById(Long id){
-        return EmployeeConverter.convertEmployeeToEmployeeDTO(employeeRepository.findById(id).get());
+    public Employee getEmployeeById(Long id){
+        return employeeRepository.findById(id).get();
     }
-    public EmployeeDTO addNewEmployee(CreateEmployeeDTO employeeDTO){
+    public EmployeeDTO addNewEmployee(CreateEmployeeDTO employeeDTO)throws ObjectAlreadyExistsException, PasswordDoesNotMatchException{
         Employee employee = null;
-        if(employeeDTO.getFirstPassword().equals(employeeDTO.getSecondPassword())){
+        if(employeeRepository.existsEmployeeByUsername(employeeDTO.getUsername())) {
+            throw new ObjectAlreadyExistsException(messageSource.getMessage("username.already.exists", null, LocaleContextHolder.getLocale()));
+        }else if(!(employeeDTO.getFirstPassword().equals(employeeDTO.getSecondPassword()))){
+            throw new PasswordDoesNotMatchException(messageSource.getMessage("password.does.not.match", null, LocaleContextHolder.getLocale()));
+        }else{
             employee = EmployeeConverter.convertCreateEmployeeDTOToEmployee(employeeDTO);
             employee.setPassword(passwordEncoder.encode(employee.getPassword()));
             employeeRepository.saveAndFlush(employee);
             return EmployeeConverter.convertEmployeeToEmployeeDTO(employee);
-        } else{
-            throw new InputMismatchException("Passwords must match");
         }
-
     }
     public EmployeeDTO updateEmployeeById(EmployeeDTO employeeDTO){
-        Employee employee = EmployeeConverter.convertEmployeeDTOToEmployee(getEmployeeById(employeeDTO.getId()));
-        employee.setId(employeeDTO.getId());
-        employee.setName(employeeDTO.getName());
-        employee.setSurname(employeeDTO.getSurname());
-        employee.setEmail(employeeDTO.getEmail());
-        if(employeeDTO.getOrderDTOList() != null) {
-            employee.setOrderList(OrderConverter.convertOrdersDTOToOrders(employeeDTO.getOrderDTOList()));
+        Employee employee = getEmployeeById(employeeDTO.getId());
+        if(employee!= null) {
+            employee.setId(employeeDTO.getId());
+            employee.setName(employeeDTO.getName());
+            employee.setSurname(employeeDTO.getSurname());
+            employee.setEmail(employeeDTO.getEmail());
+            if (employeeDTO.getOrderDTOList() != null) {
+                employee.setOrderList(OrderConverter.convertOrdersDTOToOrders(employeeDTO.getOrderDTOList()));
+            }
+            employeeRepository.saveAndFlush(employee);
+            return EmployeeConverter.convertEmployeeToEmployeeDTO(employee);
+        } else {
+            throw new NoSuchElementException(messageSource.getMessage("employee.by.id.not.found", null, LocaleContextHolder.getLocale()));
         }
-        employeeRepository.saveAndFlush(employee);
-        return EmployeeConverter.convertEmployeeToEmployeeDTO(employee);
     }
     public void deleteEmployeeById(Long id){
         employeeRepository.deleteById(id);
+    }
+    public void changeEmployeePassword(PasswordChangeDTO password, Long id) throws PasswordDoesNotMatchException{
+       Employee employee = getEmployeeById(id);
+        if (!(passwordEncoder.matches(password.getOldPassword(), employee.getPassword()))) {
+            throw new PasswordDoesNotMatchException(messageSource.getMessage("incorrect.password", null, LocaleContextHolder.getLocale()));
+
+        } else if (password.getNewPassword().equals(password.getRepeatNewPassword())) {
+            throw new PasswordDoesNotMatchException(messageSource.getMessage("new.password.does.not.match", null, LocaleContextHolder.getLocale()));
+        } else {
+            employee.setPassword(passwordEncoder.encode(password.getNewPassword()));
+            employeeRepository.saveAndFlush(employee);
+        }
+
     }
 }
