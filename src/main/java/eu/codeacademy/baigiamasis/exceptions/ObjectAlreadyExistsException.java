b3d401326a6b6be6cfadbad3cd952db9da59842f
Karolis Kersys <kersys.karolis@inbox.lt>
2023-08-02 22:08:21 +0300

    RESTful api: added custom exceptions for API

---
+++ src/main/java/eu/codeacademy/baigiamasis/exceptions/ObjectAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package eu.codeacademy.baigiamasis.exceptions;
+
+public class ObjectAlreadyExistsException extends Exception{
+    public ObjectAlreadyExistsException(String message){
+        super(message);
+    }
+}
