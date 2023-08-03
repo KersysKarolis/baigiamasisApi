5211a88ef10afab4f55e6f2caacf4b11f6a0fb18
Karolis Kersys <kersys.karolis@inbox.lt>
2023-08-03 20:19:19 +0300

    RESTful api: updated User Details method which now generates user depending on username

--- src/main/java/eu/codeacademy/baigiamasis/services/UserDetailsServiceImplementation.java
+++ src/main/java/eu/codeacademy/baigiamasis/services/UserDetailsServiceImplementation.java
@@ -1,9 +1,13 @@
 package eu.codeacademy.baigiamasis.services;
 
 import eu.codeacademy.baigiamasis.entities.Client;
+import eu.codeacademy.baigiamasis.entities.Employee;
 import eu.codeacademy.baigiamasis.enumerators.Role;
 import eu.codeacademy.baigiamasis.repositories.ClientRepository;
+import eu.codeacademy.baigiamasis.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,14 +23,23 @@ import java.util.List;
 public class UserDetailsServiceImplementation implements UserDetailsService {
     @Autowired
     ClientRepository clientRepository;
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    MessageSource messageSource;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) {
         Client client = clientRepository.findClientByUsername(username);
+
         if(client != null){
             return new User(client.getUsername(), client.getPassword(), buildSimpleGrantedAuthorities(client.getRole()));
-        } else{
-            throw new UsernameNotFoundException("Client with given username not found: " + username);
+        } else {
+            Employee employee = employeeRepository.findByUsername(username);
+            if(employee != null) {
+                return new User(employee.getUsername(), employee.getPassword(), buildSimpleGrantedAuthorities(employee.getRole()));
+            }
         }
+        throw new UsernameNotFoundException(messageSource.getMessage("username.not.found", null, LocaleContextHolder.getLocale()));
     }
     private static List<SimpleGrantedAuthority> buildSimpleGrantedAuthorities(Role role) {
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
